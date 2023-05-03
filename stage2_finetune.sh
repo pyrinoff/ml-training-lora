5aef14d246e5851847f505bbb153d26b08587412
Andrey Pyrinov <pirinoff.a@gmail.com>
2023-05-03 18:30:40 +0300

    Fix.

--- stage2_finetune.sh
+++ stage2_finetune.sh
@@ -6,7 +6,6 @@ python finetune.py --base_model 'decapoda-research/llama-7b-hf' \
   --num_epochs=10 \
   --cutoff_len=512 \
   --group_by_length \
-  --output_dir='./lora-alpaca' \
   --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
   --lora_r=16 \
   --micro_batch_size=8
